ba4d221bc034fad58e66bd659e208d22af9bb460
ironboy <thomas@nodehill.com>
2021-05-13 17:03:25 +0200

    whitespace

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import _useStates from './useStates.js';
 import _withLoop from './withLoop.js';
 import _Style from './Style.js';
-
 export * from './contextHandler.js';
 export * from './If.js';
 export const useStates = _useStates;
