b0d98a94f15a30bd844f406df370b1caead0a041
ironboy <thomas@nodehill.com>
2021-05-13 12:31:02 +0200

    scoped styling

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import _useStates from './useStates.js';
 import _withLoop from './withLoop.js';
+import _Style from './Style.js';
+
 export * from './contextHandler.js';
 export * from './If.js';
 export const useStates = _useStates;
 export const withLoop = _withLoop;
+export const Style = _Style;
