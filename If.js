6301e8e438c143d6493b25dc67e29ea3a14c0e82
ironboy <thomas@nodehill.com>
2021-05-28 01:51:12 +0200

    bugfix if with one child

--- If.js
+++ If.js
@@ -3,7 +3,10 @@ export const ElseIf = props => props.children;
 export const Else = props => props.children;
 
 export const If = props => {
-  let _elseIf = [props.children.find(x => [ElseIf, Else].includes(x.type) && (x.props.c || x.type === Else) && !props.c)];
-  let _if = props.children.filter(x => ![ElseIf, Else].includes(x.type));
+  let children = props.children;
+  !children && (children = []);
+  children.constructor !== Array && (children = [children]);
+  let _elseIf = [children.find(x => [ElseIf, Else].includes(x.type) && (x.props.c || x.type === Else) && !props.c)];
+  let _if = children.filter(x => ![ElseIf, Else].includes(x.type));
   return (props.c ? _if : _elseIf) || null;
 }
